4f1376e581d9c5fa98bbdb0a9636abadf5f96b2d
fabia <fabia@FABI>
2024-08-28 18:05:14 -0500

    Add project files.

---
+++ Laboratorio1.ApiService/Program.cs
@@ -0,0 +1,8 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //See https://aka.ns/new-console-template for more information
+        Console.WriteLine("Hello, World");
+    }
+}
