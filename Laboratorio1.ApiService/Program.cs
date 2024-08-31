a2695631c6af806583a2d705319f08efd6342f63
FABIANA GONZALEZ <fabiana.gonzalez@utp.ac.pa>
2024-08-30 20:54:07 -0500

    Laboratorio 1 terminado

--- Laboratorio1.ApiService/Program.cs
+++ Laboratorio1.ApiService/Program.cs
@@ -3,6 +3,6 @@ internal class Program
     private static void Main(string[] args)
     {
         //See https://aka.ns/new-console-template for more information
-        Console.WriteLine("Hola Mundo!!!");
+        Console.WriteLine("Hola Mundo!!");
     }
 }
